27c867dcc6b9f8276c1a592ca0401442c4e4c31e
talshuker <talshuker11@gmail.com>
2022-12-22 22:55:30 +0200

    feat (server): app listen to series requests

---
+++ server/src/server/app.js
@@ -0,0 +1,29 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}));
+
+app.get('/series');
+
+app.get('/series/:id');
+
+app.get('/series/filters');
+
+app.get('/series/commonAmongFollowing');
+
+app.get('/series/watched');
+
+app.get('/series/topRated');
+
+app.get('/series/popular');
+
+
+module.exports = app;
