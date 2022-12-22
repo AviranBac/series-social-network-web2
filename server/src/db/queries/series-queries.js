d0a852372707e5de3e3e02d7f076006d0d60eece
talshuker <talshuker11@gmail.com>
2022-12-22 23:02:10 +0200

    feat (server): adding get series mock functions

---
+++ server/src/db/queries/series-queries.js
@@ -0,0 +1,29 @@
+const Series = require('../models/series');
+
+const getAllSeries = async () => {
+
+};
+
+const getSeriesById = (id) => {
+
+};
+
+const getPopularSeries = () => {
+
+};
+
+const getTopRatedSeries = () => {
+
+};
+
+const getWatchedSeries = () => {
+
+};
+
+module.exports = {
+    getAllSeries,
+    getSeriesById,
+    getPopularSeries,
+    getTopRatedSeries, 
+    getWatchedSeries
+};
