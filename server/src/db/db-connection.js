d773722580b3d7fbeed870a0d234ae7646d4d23f
amittes <amitt886@gmail.com>
2022-12-23 15:18:06 +0200

    feat: starting to create reqeusts for tmdb api

--- server/src/db/db-connection.js
+++ server/src/db/db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const initConnection = async () => {
-    await mongoose.connect('mongodb://localhost:27017/');
+    await mongoose.connect('mongodb://127.0.0.1:27017/');
     console.log('Connected to mongoDB successfully');
 };
 
--- server/src/server/app.js
+++ server/src/server/app.js
@@ -1,8 +1,11 @@
+const axios = require('axios');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const websiteUrl = "https://api.themoviedb.org/3/";
+const apiKey = "4287ce9fa98248ad849cf2e6502ff51a";
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use(cors({
@@ -11,7 +14,39 @@ app.use(cors({
     optionSuccessStatus: 200
 }));
 
-app.get('/series');
+
+app.get('/series', async (req, res) => {
+    let series = getMostPopularSeries();
+    console.log(series);
+  });
+  
+  async function getMostPopularSeries() {
+    try {
+      let seriesList = [];
+      let page = 1;
+      let totalPages = 1;
+  
+      while (seriesList.length < 50 && page <= totalPages) {
+        const response = await axios.get(websiteUrl, {
+          params: {
+            api_key: apiKey,
+            language: 'en-US',
+            page: page
+          }
+        });
+  
+        seriesList = [...seriesList, ...response.data.results];
+        page = response.data.page + 1;
+        totalPages = response.data.total_pages;
+      }
+  
+      return seriesList;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+  
 
 app.get('/series/:id');
 
