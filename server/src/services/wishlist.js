c6afb7311a2fe715e9c5be609bb22f012ea75325
amittes <amitt886@gmail.com>
2022-12-31 21:22:49 +0200

    fix: after cr

--- server/src/services/wishlist.js
+++ server/src/services/wishlist.js
@@ -4,15 +4,7 @@ const Series = require("../db/mongo/models/series");
 const getUserWishlist = async (email) => {
 
     const wishlist = await WishLists.findOne({ email: email}).exec();
-
-    // let seriesWishlist = [];
-    // { $in: arrayOfIds }
-    // // for (const series_id of wishlist.series_ids ) {
-    // //     const seriesDetails = await Series.findById(series_id).exec();
-    // //     seriesWishlist.push(seriesDetails);
-    // // }
-    const seriesWishlist = await Series.find({_id: { $in: wishlist.series_ids}}).exec();
-    return seriesWishlist;
+    return Series.find({_id: { $in: wishlist.series_ids}}).exec();
 };
 
 module.exports = {
