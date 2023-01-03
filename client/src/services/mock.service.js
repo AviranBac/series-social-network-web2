baba9be3001793362b93144e8beef1d3fc9fb8b9
Aviran Bachar <aviranbac1@gmail.com>
2023-01-04 00:51:12 +0200

    fix(Pagination Table): Removing leftovers from mocking

--- client/src/services/mock.service.js
+++
@@ -1,8 +0,0 @@
-const totalElements = 152;
-const content =  Array.from({length: totalElements}, (_, i) => i + 1)
-    .map(index => ({ a: index, b: index, c: index, d: index, e: index }));
-export const mockResponse = {
-    content,
-    totalPages: Math.ceil(totalElements / 10),
-    totalElements
-}
