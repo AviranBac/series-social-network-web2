32df3616b748ef85875e19330c3e8cc51083c941
amittes <amitt886@gmail.com>
2023-01-04 21:49:50 +0200

    creating home page, getting series from the server

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -4,13 +4,24 @@ import { useEffect, useState } from 'react';
 
 const Home = () => {
 
-  const [series, setSeries] = useState([]);
+  const [recommendedSeries, setRecommendedSeries] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('http://localhost:8080/series');
       const data = await response.json();
-      setSeries(data);
+      setRecommendedSeries(data);
+    }
+    fetchData();
+  }, []);
+
+  const [mostPopularSeries, setMostPopularSeries] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:8080/series');
+      const data = await response.json();
+      setMostPopularSeries(data);
     }
     fetchData();
   }, []);
@@ -33,12 +44,12 @@ const Home = () => {
                          <h5>Series You May Like</h5>
 
              <MDBRow className='row-cols-1 row-cols-md-5 g-3'  style={rowStyle}>
-                {series.map((series) => <SeriesCard series={series}/>)}
+                {recommendedSeries.map((series) => <SeriesCard series={series}/>)}
             </MDBRow>
 
             <h5>Most Popular Series</h5>
             <MDBRow className='row-cols-1 row-cols-md-5 g-3'  style={{ border: '1px solid #ccc'}}>
-            {series.map((series) => <SeriesCard series={series}/>)}
+            {mostPopularSeries.map((series) => <SeriesCard series={series}/>)}
             </MDBRow>
         </div>
            
