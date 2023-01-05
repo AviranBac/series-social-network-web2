307a30da7938e022bb1389af30b55e4c3f008302
talshuker <talshuker11@gmail.com>
2023-01-05 23:03:50 +0200

    update image url in init\models

--- server/src/init/models/episodes.js
+++ server/src/init/models/episodes.js
@@ -2,7 +2,10 @@ const Episodes = require('../../db/mongo/models/episode');
 
 const insertEpisodes = async (series, season, episodes) => {
     const episodesWithRefIds = episodes.map((episode) => {
-        return { ...episode, season_id: season._id, still_path: `https://image.tmdb.org/t/p/w500${episode.still_path}`}
+        return { 
+            ...episode, 
+            season_id: season._id, 
+            still_path: episode.still_path ? `https://image.tmdb.org/t/p/w500${episode.still_path}` : undefined}
     });
 
     let response;
