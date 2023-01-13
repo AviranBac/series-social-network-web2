223041d590c0e517a32e8d54714ae9724d8d2aef
talshuker <talshuker11@gmail.com>
2023-01-13 13:22:06 +0200

    feat: add spacing

--- client/src/components/seriesFilters/SeriesFilters.js
+++ client/src/components/seriesFilters/SeriesFilters.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { MDBInputGroup, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
 import seriesService from '../../services/series.service';
+import { Container } from 'react-bootstrap';
 
-const filtersType = Object.freeze({ GENRES: "genres", STATUSES: "statuses"});
+const filtersType = Object.freeze({ GENRES: "genres", STATUSES: "statuses" });
 
 const SeriesFilters = ({ setSeriesFilters }) => {
     const [seriesFilterTypes, setSeriesFilterTypes] = useState([]);
@@ -22,8 +23,8 @@ const SeriesFilters = ({ setSeriesFilters }) => {
 
     const setFilters = (e, type) => {
         const { value, checked } = e.target;
-        const updatedType = checked ? 
-        ({ [type]: [...(userFilters[type]), value] }) : ({ [type]: userFilters[type].filter((filterVal) => filterVal !== value) });
+        const updatedType = checked ?
+            ({ [type]: [...(userFilters[type]), value] }) : ({ [type]: userFilters[type].filter((filterVal) => filterVal !== value) });
         setUserFilters({
             ...userFilters,
             ...updatedType
@@ -31,23 +32,23 @@ const SeriesFilters = ({ setSeriesFilters }) => {
     };
 
     return (
-        <> 
+        <Container>
             <MDBInputGroup>
-                <MDBInput label='Search by name' onChange={(e) => { setUserFilters({ ...userFilters, name: e.target.value}) }} />
+                <MDBInput label='Search by name' onChange={(e) => { setUserFilters({ ...userFilters, name: e.target.value }) }} />
             </MDBInputGroup>
-            <h6 className="mt-2 text-primary fw-bold text-decoration-underline">Filter by genres</h6>
+            <h6 className="mt-4 text-primary fw-bold text-decoration-underline">Filter by genres</h6>
             {
                 (seriesFilterTypes?.genres || []).map((genre) =>
-                <MDBCheckbox key={genre._id} value={genre.name} label={genre.name} onChange={(e) => setFilters(e, filtersType.GENRES)}/>
+                    <MDBCheckbox key={genre._id} value={genre.name} label={genre.name} onChange={(e) => setFilters(e, filtersType.GENRES)} />
                 )
             }
-            <h6 className="mt-2 text-primary fw-bold text-decoration-underline">Filter by statuses</h6>
+            <h6 className="mt-4 text-primary fw-bold text-decoration-underline">Filter by statuses</h6>
             {
-                (seriesFilterTypes?.statuses || []).map((status) => 
-                <MDBCheckbox key={status} value={status} label={status} onChange={(e) => setFilters(e, filtersType.STATUSES)}/>
+                (seriesFilterTypes?.statuses || []).map((status) =>
+                    <MDBCheckbox key={status} value={status} label={status} onChange={(e) => setFilters(e, filtersType.STATUSES)} />
                 )
             }
-        </>
+        </Container>
     )
 };
 
