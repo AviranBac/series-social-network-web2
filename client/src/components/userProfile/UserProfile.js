2e2142774692dd0d44dde4215c9eac3896c743be
talshuker <talshuker11@gmail.com>
2023-01-13 17:57:30 +0200

    feat: basic tab bar

--- client/src/components/userProfile/UserProfile.js
+++ client/src/components/userProfile/UserProfile.js
@@ -1,7 +1,33 @@
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/auth.selectors";
+import { Tab, Tabs } from 'react-bootstrap';
+import Followers from "./Followers";
+import Followings from './Followings';
+import './userProfile.module.css';
+
 const UserProfile = () => {
+    const user = useSelector(selectUser);
 
     return (
-        <p>User profile page!</p>
+        <>
+            <h2 className="mt-4 text-primary text-center fw-bold text-decoration-underline">{user.email}</h2>
+            <Tabs
+                defaultActiveKey="Wishlist"
+                className="mb-3"
+                justify
+            >
+                <Tab eventKey="Wishlist" title="Wishlist">
+                </Tab> 
+                <Tab eventKey="Watchlist" title="Watchlist">
+                </Tab>
+                <Tab eventKey="Following" title="Following">
+                    <Followings />
+                </Tab>
+                <Tab eventKey="Followers" title="Followers">
+                    <Followers />
+                </Tab>
+            </Tabs>
+        </>
     );
 };
 
